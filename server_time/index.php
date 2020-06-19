5d36e2d81ad163a44aba066e67c61a5a436a557a
cuong6218 <cuong6218@domain.com>
2020-06-19 15:11:00 +0700

    update file server_time

--- server_time/index.php
+++ server_time/index.php
@@ -1,2 +1,11 @@
-<?php
-echo 'Xin chao';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Current Time</title>
+</head>
+<body>
+    <h1>Bây giờ là:<?php echo date('Y-M-d h:m:s'); ?> </h1>
+</body>
+</html>
